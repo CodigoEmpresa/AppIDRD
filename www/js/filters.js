9acd9edec32c51ff023456fb199cf3316845617d
Unknown <jonatancastro1@gmail.com>
2016-12-16 11:09:27 -0500

    Adelanto test multiplataformas

--- www/js/filters.js
+++ www/js/filters.js
@@ -1,7 +1,8 @@
 angular.module('starter.filters', [])
 .filter('dateToISO', function() {
 	return function(input) {
-		input = new Date(input).toISOString();
-		return input;
+		var t = input.split(/[- :]/);
+		var date = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
+        return date;
 	};
 });
